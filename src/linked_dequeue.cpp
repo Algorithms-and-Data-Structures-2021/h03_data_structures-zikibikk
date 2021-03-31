a68a60fd028d6a1c1eefbfe5156d6bc723a9d205
Alina <bikkinina.aa@mail.ru>
2021-03-31 20:36:20 +0300

    Now all it works!!!

--- src/linked_dequeue.cpp
+++ src/linked_dequeue.cpp
@@ -4,66 +4,110 @@
 
 namespace itis {
 
-void LinkedDequeue::Enqueue(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::Enqueue(Element e) {
+        auto *node = new DoublyNode(e, nullptr, back_);
 
-void LinkedDequeue::EnqueueFront(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+        if (size_ == 0) {
+            front_ = node;
+        } else {
+            back_->previous = node;
+        }
+        back_ = node;
+        size_++;
+    }
 
-void LinkedDequeue::Dequeue() {
-  if (size_ == 0) {
-    throw std::logic_error("cannot not dequeue from empty queue");
-  }
+    void LinkedDequeue::EnqueueFront(Element e) {
 
-  // TODO: напишите здесь свой код ...
-}
+        auto *node = new DoublyNode(e, nullptr, front_);
 
-void LinkedDequeue::DequeueBack() {
-  if (size_ == 0) {
-    throw std::logic_error("cannot not dequeue from empty queue");
-  }
+        if (size_ == 0) {
+            front_ = node;
+            back_ = node;
+        } else {
+            front_->next = node;
+            front_ = node;
+        }
+        size_++;
+    }
 
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::Dequeue() {
+        if (size_ == 0) {
+            throw std::logic_error("cannot not dequeue from empty queue");
+        }
+        if (size_ == 1) {
+            delete front_;
+            front_ = nullptr;
+            back_ = nullptr;
+        } else {
+            auto *node = front_->previous;
+            delete front_;
+            front_ = node;
+            node->next = nullptr;
+        }
+        size_--;
+    }
 
-void LinkedDequeue::Clear() {
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::DequeueBack() {
+        if (size_ == 0) {
+            throw std::logic_error("cannot not dequeue from empty queue");
+        }
+        if (size_ == 1) {
+            delete front_;
+            front_ = nullptr;
+            back_ = nullptr;
+        } else {
+            auto *node = back_->next;
+            delete back_;
+            back_ = node;
+            node->previous = nullptr;
+        }
+        size_--;
+
+    }
+
+    void LinkedDequeue::Clear() {
+        for (DoublyNode *current_node = front_; current_node != nullptr;) {
+            auto delete_node = current_node;
+            current_node = current_node->previous;
+            delete delete_node;
+        }
+        front_ = nullptr;
+        back_ = nullptr;
+        size_ = 0;
+    }
 
 // === РЕАЛИЗОВАНО ===
 
-LinkedDequeue::~LinkedDequeue() {
-  Clear();
-}
+    LinkedDequeue::~LinkedDequeue() {
+        Clear();
+    }
 
-std::optional<Element> LinkedDequeue::front() const {
-  return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
-}
+    std::optional<Element> LinkedDequeue::front() const {
+        return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
+    }
 
-std::optional<Element> LinkedDequeue::back() const {
-  return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
-}
+    std::optional<Element> LinkedDequeue::back() const {
+        return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
+    }
 
-bool LinkedDequeue::IsEmpty() const {
-  return size_ == 0;
-}
+    bool LinkedDequeue::IsEmpty() const {
+        return size_ == 0;
+    }
 
-int LinkedDequeue::size() const {
-  return size_;
-}
+    int LinkedDequeue::size() const {
+        return size_;
+    }
 
 // === НЕОБХОДИМО ДЛЯ ТЕСТИРОВАНИЯ ===
 
-std::ostream &operator<<(std::ostream &os, const LinkedDequeue &queue) {
-  os << "size: " << queue.size_ << '\n';
-  for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->previous) {
-    if (current_node == queue.front_) os << "[FRONT] ";
-    if (current_node == queue.back_) os << "[BACK] ";
-    os << enum2str(current_node->data) << '\n';
-  }
-  return os;
-}
+    std::ostream &operator<<(std::ostream &os, const LinkedDequeue &queue) {
+        os << "size: " << queue.size_ << '\n';
+        for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->previous) {
+            if (current_node == queue.front_) os << "[FRONT] ";
+            if (current_node == queue.back_) os << "[BACK] ";
+            os << enum2str(current_node->data) << '\n';
+        }
+        return os;
+    }
 
 }  // namespace itis
